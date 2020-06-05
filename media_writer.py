a392ffd283995265d5b91532c63c6b4c0e3ee4f5
Brian Masney <masneyb@onstation.org>
2020-06-04 20:06:57 -0400

    add single space between elements so they can be split if needed
    Signed-off-by: Brian Masney <masneyb@onstation.org>

--- media_writer.py
+++ media_writer.py
@@ -504,13 +504,13 @@ class Html:
 
         if len(links) <= 11:
             return self.__get_expandable_element(label.replace(" ", "_").lower(),
-                                                 "%s: %s" % (label, "".join(links)), None,
+                                                 "%s: %s" % (label, " ".join(links)), None,
                                                  "header_links", None)
 
         more_label = "+%s more" % (len(links) - 10)
         return self.__get_expandable_element(label.replace(" ", "_").lower(),
-                                             "%s: %s" % (label, "".join(links[0:10])),
-                                             "%s: %s" % (label, "".join(links)),
+                                             "%s: %s" % (label, " ".join(links[0:10])),
+                                             "%s: %s" % (label, " ".join(links)),
                                              "header_links", more_label)
 
     def __get_tag_event_links(self, tag):
