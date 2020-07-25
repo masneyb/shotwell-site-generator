7b47c33026e47ae5209650e612fc0cb84f96ce8d
Brian Masney <masneyb@onstation.org>
2020-07-24 20:37:43 -0400

    add license to header
    Signed-off-by: Brian Masney <masneyb@onstation.org>

--- search.js
+++ search.js
@@ -1,3 +1,22 @@
+/*
+ * Generic functions for searching for media that's used by the search and
+ * screensaver pages.
+ *
+ * Copyright (C) 2020 Brian Masney <masneyb@onstation.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 function getQueryParameter(name, defaultValue) {
   var urlParams = new URLSearchParams(window.location.search);
   return urlParams.has(name) ? urlParams.get(name) : defaultValue;
