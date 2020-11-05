f65f4dae0fc0b53b286a34ea451b41488163cd7f
Brian Masney <masneyb@onstation.org>
2020-11-04 19:31:49 -0500

    add support for saved searches
    This provides a default saved_searches.js file. You can overwrite this
    in your library and include URLs to searches of interest. This file in
    your library won't be overwritten.
    
    Signed-off-by: Brian Masney <masneyb@onstation.org>

---
+++ saved_searches.js
@@ -0,0 +1,13 @@
+var _savedSearches = [
+    {
+        "label": "Recently added",
+        "url": "search.html?sortby=created"
+    },
+    {
+        "label": "Taken on this day",
+        "url": "search.html?search=Date%2Cwas taken on this day"
+    }
+];
+function getSavedSearches() {
+  return _savedSearches;
+}
