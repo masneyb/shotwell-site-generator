38f083f467a8336b4d73e369f242e9cce8b8d0e1
Brian Masney <masneyb@onstation.org>
2021-04-25 15:58:57 -0400

    convert license text to SPDX tags
    Signed-off-by: Brian Masney <masneyb@onstation.org>

--- lint.sh
+++ lint.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2020-2021 Brian Masney <masneyb@onstation.org>
 
 pylint-3 --disable=missing-module-docstring,missing-function-docstring,missing-class-docstring,no-self-use *.py
--- media_writer_html.py
+++ media_writer_html.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
-
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2020-2021 Brian Masney <masneyb@onstation.org>
+#
 # Exports a static HTML view of your shotwell photo/video library.
-#
-# Copyright (C) 2020 Brian Masney <masneyb@onstation.org>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import html
 import os
