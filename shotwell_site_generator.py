d60963318d401c6fe233f628c359401feb1a3f04
Brian Masney <masneyb@onstation.org>
2021-12-02 06:36:37 -0500

    add static-site.html symlink
    Signed-off-by: Brian Masney <masneyb@onstation.org>

--- shotwell_site_generator.py
+++ shotwell_site_generator.py
@@ -84,6 +84,10 @@ def process_photos(options):
             os.unlink(media_symlink)
         os.symlink(options.input_media_path, media_symlink)
 
+    static_symlink = os.path.join(options.dest_directory, "static-site.html")
+    if not os.path.islink(static_symlink):
+        os.symlink("media/index.html", static_symlink)
+
     photos = media_writer_html.Html(all_media, options.dest_directory, options.title,
                                     options.years_prior_are_approximate,
                                     options.max_media_per_page, options.expand_all_elements,
