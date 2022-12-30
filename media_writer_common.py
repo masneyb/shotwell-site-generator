3fbf9f7d067da507442dfa296c6e851dac2d6bb3
Brian Masney <masneyb@onstation.org>
2022-12-30 07:32:05 -0500

    clean up the date range when the beginning and end date are the same
    Signed-off-by: Brian Masney <masneyb@onstation.org>

--- media_writer_common.py
+++ media_writer_common.py
@@ -59,6 +59,12 @@ class CommonWriter:
         max_parts = self._get_date_parts(max_timestamp)
 
         if min_parts != max_parts and min_parts["month"]:
+            if min_parts["year"] == max_parts["year"] and \
+               min_parts["month"] == max_parts["month"] and \
+               min_parts["day"] == max_parts["day"]:
+                return "%s %s, %s" % \
+                       (min_parts["month"], min_parts["day"], min_parts["year"])
+
             if min_parts["year"] == max_parts["year"] and min_parts["month"] == max_parts["month"]:
                 return "%s %s-%s, %s" % \
                        (min_parts["month"], min_parts["day"], max_parts["day"], min_parts["year"])
