c878207615237b3570947e4bbf4baa647f8e50b5
Brian Masney <masneyb@onstation.org>
2023-11-19 06:35:16 -0500

    correct pylint warning
    Fix the following warning:
    
        Command line:1:0: R0022: Useless option value for '--disable',
        'no-self-use' was moved to an optional extension, see
        https://pylint.readthedocs.io/en/latest/whatsnew/2/2.14/summary.html#removed-checkers.
        (useless-option-value)
    
    Signed-off-by: Brian Masney <masneyb@onstation.org>

--- lint.sh
+++ lint.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (C) 2020-2021 Brian Masney <masneyb@onstation.org>
 
-pylint-3 --disable=consider-using-f-string,missing-module-docstring,missing-function-docstring,missing-class-docstring,no-self-use *.py
+pylint-3 --disable=consider-using-f-string,missing-module-docstring,missing-function-docstring,missing-class-docstring *.py
 
 if [ ! -d node_modules ] ; then
 	npm install eslint eslint-config-airbnb-base eslint-plugin-html eslint-plugin-import --save-dev
