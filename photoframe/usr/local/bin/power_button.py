666c6e9027bb83fa0caa15c815413778283ce305
Brian Masney <masneyb@onstation.org>
2024-04-18 20:50:46 -0400

    improve button debounce time
    Coding wise, this still doesn't feel like the best way to do this with
    gpiozero, however this will do for now. Pressing the button though feels
    more natural now.
    
    Signed-off-by: Brian Masney <masneyb@onstation.org>

--- photoframe/usr/local/bin/power_button.py
+++ photoframe/usr/local/bin/power_button.py
@@ -3,17 +3,20 @@
 import argparse
 import subprocess
 import sys
+import time
 from gpiozero import Button
 
 def execute_command(command):
     subprocess.run([command], stdout=sys.stdout, stderr=sys.stderr, check=True)
 
 def wait_for_button_press(args):
-    button = Button(args.pin)
+    button = Button(pin=args.pin, pull_up=True, bounce_time=0.1)
 
     while True:
-        if button.wait_for_press():
+        if button.is_pressed:
             execute_command(args.toggle_command)
+            time.sleep(0.1)
+        button.wait_for_press(timeout=None)
 
 ARGPARSER = argparse.ArgumentParser()
 ARGPARSER.add_argument("--pin", type=int, default=17)
