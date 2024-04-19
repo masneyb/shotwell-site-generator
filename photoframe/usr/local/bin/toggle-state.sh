0e17e8b19077f09c3162a11dd7519651c9abda80
Brian Masney <masneyb@onstation.org>
2024-04-18 20:49:44 -0400

    use 'xset q' to determine if the monitor is on
    Signed-off-by: Brian Masney <masneyb@onstation.org>

--- photoframe/usr/local/bin/toggle-state.sh
+++ photoframe/usr/local/bin/toggle-state.sh
@@ -3,7 +3,7 @@
 if [ -d /sys/class/backlight/rpi_backlight ] ; then
 	DISABLED=$(cat /sys/class/backlight/rpi_backlight/bl_power)
 else
-	DISABLED=$(xrandr | grep "HDMI-1 connected primary (normal" | wc -l)
+	DISABLED=$(xset q | grep "Monitor is Off" | wc -l)
 fi
 
 if [ "${DISABLED}" = "1" ] ; then
