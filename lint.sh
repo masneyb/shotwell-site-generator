7c28169c580d6d87a4865ce990f9a521926cef21
Brian Masney <masneyb@onstation.org>
2024-12-01 19:12:50 -0500

    update global pylint settings. Will eventually dial these back at some point in the future
    Signed-off-by: Brian Masney <masneyb@onstation.org>

--- lint.sh
+++ lint.sh
@@ -2,7 +2,9 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (C) 2020-2024 Brian Masney <masneyb@onstation.org>
 
-pylint-3 --disable=consider-using-f-string,missing-module-docstring,missing-function-docstring,missing-class-docstring *.py
+set -x
+
+pylint-3 *.py
 
 if [ ! -d node_modules ] ; then
 	npm install eslint eslint-config-airbnb-base eslint-plugin-html eslint-plugin-import --save-dev
