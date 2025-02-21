b2fe948e5f25e2fadc3f3611a44f94207ce1f60e
Brian Masney <masneyb@onstation.org>
2025-02-21 06:34:23 -0500

    reverse list in ascending order

--- photobook-helper/pdf-scripts/create-files-txt.sh
+++ photobook-helper/pdf-scripts/create-files-txt.sh
@@ -2,4 +2,4 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # Copyright (C) 2020-2024 Brian Masney <masneyb@onstation.org>
 
-find . -name media.csv | sed "s/\/media.csv//" | xargs -iblah echo "~/src/shotwell-site-generator/photobook-helper/csv-to-filelist.sh < blah/media.csv > blah/files.txt" | sh -vx
+find . -name media.csv | sed "s/\/media.csv//" | xargs -iblah echo "~/src/shotwell-site-generator/photobook-helper/csv-to-filelist.sh < blah/media.csv | tac > blah/files.txt" | sh -vx
