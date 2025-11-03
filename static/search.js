86b1ece79ed31e5de7f0b0dc2a64477439b3b0fd
Brian Masney <masneyb@onstation.org>
2025-11-02 20:26:16 -0500

    update copyright year
    Signed-off-by: Brian Masney <masneyb@onstation.org>

--- static/search.js
+++ static/search.js
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: AGPL-3.0-only
- * Copyright (C) 2020-2024 Brian Masney <masneyb@onstation.org>
+ * Copyright (C) 2020-2025 Brian Masney <masneyb@onstation.org>
  */
 
 /*
