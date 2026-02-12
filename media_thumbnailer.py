f497575a42aecff59fb7a2973384e3489587ea19
Brian Masney <masneyb@onstation.org>
2026-02-12 06:11:20 -0500

    clean up FIXME comment
    Signed-off-by: Brian Masney <masneyb@onstation.org>

--- media_thumbnailer.py
+++ media_thumbnailer.py
@@ -519,9 +519,9 @@ class Thumbnailer:
             complex_filter += (f"select=not(mod(n-1\\,{select_frames}))[skip];"
                                f"[skip]setpts=N/({pts}*TB)[fps];[fps]")
 
-        # FIXME - adjustments.shadows, adjustments.exposure, referenced, straighten.angle,
-        # adjustments.expansion are implemented below in __get_imagemagick_transformation_cmd()
-        # and also need to be implemented here.
+        # FIXME - adjustments.exposure, adjustments.saturation, adjustments.shadows,
+        # straighten.angle, adjustments.expansion are implemented below in
+        # __get_imagemagick_transformation_cmd() and also need to be implemented here.
 
         if transformations and "crop.left" in transformations:
             # For the motion photos, the video resolution is different than the image
